ba8cd0c0797e64824e021d302799d063e6399e73
Fisayo <fisayofasuyi@yahoo.com>
2022-09-16 19:05:42 +0100

    commited

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=fisayo1234
+export POSTGRES_PASSWORD=fisayo1234
+export POSTGRES_HOST=mydb0893.cikd4oyez5z6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=aws-s3-bucket-1993
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
